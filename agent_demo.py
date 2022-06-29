ff3f8f2259445351953c8f7f59215ff1fd09782b
paul.mattes <paul.mattes@student.hpi.de>
2022-06-29 14:13:51 +0200

    more significant boost stuff

--- agent_demo.py
+++ agent_demo.py
@@ -1,32 +1,32 @@
 #!/bin/python
 import time
 import gym, gym_mupen64plus
-from gym_mupen64plus.envs.MarioKart64.discrete_envs import DiscreteActions
+from gym_mupen64plus.envs.mupen64plus_env import COUNTPEROP
+
+env = gym.make('Mario-Kart-Luigi-Raceway-v0')
+# for k in range(20):
+# COUNTPEROP = k
 
 render = False
-
-env = gym.make('Mario-Kart-Discrete-Luigi-Raceway-v0')
 env.reset()
 if render:
     env.render()
 
 print("NOOP waiting for green light")
 for i in range(100):
-    (obs, rew, end, info) = env.step(0) # NOOP until green light
+    (obs, rew, end, info) = env.step([0, 0, 0, 0, 0]) # NOOP until green light
     if render:
         env.render()
 
 print("GO! ...drive straight as fast as possible...")
 start = time.time()
 for i in range(1000):
-    # if i != 0 and i % 10 == 0:
-    #     start = end
-    (obs, rew, end, info) = env.step(1) # Drive straight
+    (obs, rew, end, info) = env.step([0, 0, 1, 0, 0]) # Drive straight
     if render:
         env.render()
 end = time.time()
 print(1000 / (end - start), "frames per second, total:", (end-start), "seconds")
-print((end - start), "seconds for 1000 steps")
+print(end - start, " seconds per 1000 steps")
 
 # print("Doughnuts!!")
 # for i in range(10000):
@@ -36,7 +36,7 @@ print((end - start), "seconds for 1000 steps")
 #     (obs, rew, end, info) = env.step([-80, 0, 0, 0, 0]) # Hard-left doughnuts!#
 #     env.render()
 
-input("Press <enter> to exit... ")
+# input("Press <enter> to exit... ")
 
 env.close()
 
--- gym_mupen64plus/envs/mupen64plus_env.py
+++ gym_mupen64plus/envs/mupen64plus_env.py
@@ -54,6 +54,7 @@ SCR_W = 320
 SCR_H = 240
 SCR_D = 3
 
+COUNTPEROP = 1
 MILLISECOND = 1.0 / 1000.0
 
 IMAGE_HELPER = ImageHelper()
@@ -66,11 +67,16 @@ class Mupen64PlusEnv(gym.Env):
     __metaclass__ = abc.ABCMeta
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, low_res_mode=True, benchmark=True):
+    # def __init__(self, benchmark=True, res_w=160, res_h=120):
+    def __init__(self, benchmark=True, res_w=320, res_h=240):
         global SCR_W, SCR_H
-        if not low_res_mode and not benchmark:
-            SCR_W = 640
-            SCR_H = 240
+        # if not low_res_mode and not benchmark:
+        #     SCR_W = 640
+        #     SCR_H = 480
+        SCR_W, SCR_H = res_w, res_h
+        self.res_w = res_w
+        self.res_h = res_h
+        
         self.viewer = None
         self.benchmark = benchmark
         self.reset_count = 0
@@ -97,7 +103,8 @@ class Mupen64PlusEnv(gym.Env):
             self.xvfb_process, self.emulator_process = \
                 self._start_emulator(rom_name=self.config['ROM_NAME'],
                                      gfx_plugin=self.config['GFX_PLUGIN'],
-                                     input_driver_path=self.config['INPUT_DRIVER_PATH'])
+                                     input_driver_path=self.config['INPUT_DRIVER_PATH'],
+                                     res_w=SCR_W, res_h=SCR_H, res_d=SCR_D)
 
         # TODO: Test and cleanup:
         # May need to initialize this after the DISPLAY env var has been set
@@ -314,13 +321,14 @@ class Mupen64PlusEnv(gym.Env):
             cprint(msg, 'red')
             raise Exception(msg)
         
+        print("got COUNTPEROP", COUNTPEROP)
         benchmark_options = [
                 "--nospeedlimit",
-                "--set", "Core[CountPerOp]=1",
-                "--set", "Video-Rice[ScreenUpdateSetting]=3",
-                "--set", "Video-Rice[FastTextureLoading]=1",
-                "--set", "Video-Rice[TextureQuality]=2",
-                "--set", "Video-Rice[ColorQuality]=1",
+                "--set", f"Core[CountPerOp]={COUNTPEROP}",
+                # "--set", "Video-Rice[ScreenUpdateSetting]=3",
+                # "--set", "Video-Rice[FastTextureLoading]=1",
+                # "--set", "Video-Rice[TextureQuality]=2",
+                # "--set", "Video-Rice[ColorQuality]=1",
         ]
 
         cmd = [self.config['MUPEN_CMD'],
