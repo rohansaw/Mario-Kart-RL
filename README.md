47f4731b6f23c1cc0eafd2b46c0a4b0bb7e17ac4
rohan.sawahn <rohan.sawahn@student.hpi.de>
2022-06-29 16:37:15 +0200

    added simple sb3 setup

--- README.md
+++ README.md
@@ -10,3 +10,5 @@
 
 However, this currently gives no access to bash out, if you want to see the program output run `docker run -it --name <image_name> -p 5900 --mount source="$(pwd)/gym_mupen64plus/ROMs",target=/src/gym-mupen64plus/gym_mupen64plus/ROMs,type=bind <image_name>:<tag_name> python gym-mupen64plus/agent.py`
 
+## Using stable baselines agent
+Follow SB installation instructions (here)[https://stable-baselines.readthedocs.io/en/master/guide/install.html]
---
+++ sb_agent.py
@@ -0,0 +1,18 @@
+import gym, gym_mupen64plus
+
+from stable_baselines3 import A2C
+from stable_baselines3.common.env_checker import check_env
+
+# Parallel environments
+env = gym.make('Mario-Kart-Discrete-Luigi-Raceway-v0')
+check_env(env)
+
+model = A2C("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=25000)
+model.save("a2c_cartpole")
+
+obs = env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
