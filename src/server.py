90198a9cebf80f75329916023b113a0e7fd19a98
lasklu <lukas.laskowski@student.hpi.de>
2022-08-02 19:13:11 +0200

    adopting python server for mss

--- src/server.py
+++ src/server.py
@@ -1,21 +1,11 @@
+from urllib.parse import urlparse, parse_qs
+import numpy as np
 import mss
 from pythonServer import PythonServer
+from http.server import SimpleHTTPRequestHandler
 import time
 import sys
 from http.server import HTTPServer
-print("HSJ")
-print("HHSKDHKJSHDKJS")
-print("TEST")
-print("HSD")
-print("HSD")
-print("HSD")
-#from urllib.parse import urlparse, parse_qs
-print("1")
-print("2")
-print("3")
-print("4")
-print("5")
-#import numpy as np
 HOST_NAME = "0.0.0.0"
 PORT = 8070
 print(sys.version)
@@ -23,10 +13,38 @@ print("HJDW SKJ ")
 
 
 print("HSHDKJSHSK")
-#mss_grabber = mss.mss()
+mss_grabber = mss.mss()
 print("SHDS")
 time.sleep(2)
 print("S .  ")
+
+
+class PythonServer(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        print("SS")
+        top = None
+        left = None
+        width = None
+        height = None
+        if self.path == '/image':
+            print("S")
+            if '?' in self.path:
+                path, tmp = self.path.split('?', 1)
+                qs = parse_qs(urlparse.parse_qs(tmp).query)
+                top = qs['top'][0]
+                left = qs['left'][0]
+                width = qs['width'][0]
+                height = qs['height'][0]
+            image_array = np.array(mss_grabber.grab(), dtype=np.uint8)
+            # {"top": top,
+            # "left": left,
+            #                                "width": width,
+            #                                "height": height})
+            #      dtype=np.uint8)
+            # print(image_array)
+            self.send_response(200, "OK")
+            self.end_headers()
+            self.wfile.write(bytes(image_array, "utf-8"))
 
 
 print("HA")
